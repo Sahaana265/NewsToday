98ab1a8ea72e7bc4f3f4190f6c3e355334ec5f01
Sahaana265 <147255249+Sahaana265@users.noreply.github.com>
2026-03-30 19:39:10 +0530

    Update NewsItems.jsx

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -2,7 +2,7 @@ import image from "../assets/hero.png"
 const NewsItems = ({title, description, src, url}) =>{
     return (
        <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2 " style={{maxWidth:"345px"}} >
-  <img src={src ? src : image} style={{height:"200px", width:"330px"}} classNameName="card-img-top" alt="..."/>
+  <img src={src ? src : image} style={{height:"200px", width:"328px"}} classNameName="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title.slice(0,50)}</h5>
     <p className="card-text">{description?description.slice(0,90):"Here's another look"}</p>
